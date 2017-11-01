f10c3f21c1b0559e80c5337a640a5b7d487d5422
tintoll <blue@gmail.com>
2017-11-01 13:29:16 +0900

    weather sample

---
+++ WeatherProject/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import WeatherProject from './WeatherProject';
+
+
+AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
